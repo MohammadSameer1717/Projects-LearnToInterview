4b783c8c9e2be1d8b2c7202fc66f8ed264ee13bf
MohammadSameer1717 <mohammadsameer0893@gmail.com>
2025-11-16 02:59:17 +0530

    Projects-LearnToInterview-01

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,16 @@
 import express from "express";
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const app = express()
 
+console.log(process.env.PORT)
+console.log(process.env. DB_URL)
+
+
 app.get("/", (req, res) => {
-    res.status(200).json({msg:"success from api"})
+    res.status(200).json({msg:"success from Backend 123424"});
 });
 
-app.listen(3000, () => console.log("Server is running on port 3000"))
+app.listen(3000, () => console.log("Server is running on port 3000"));
