2d208974c9efe294a4630e5c945ae9b9f48dc33f
MohammadSameer1717 <mohammadsameer0893@gmail.com>
2025-11-18 00:43:05 +0530

    Projects-LearnToInterview-03

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,29 @@
 import express from "express";
-import { ENV } from "./src/lib/env.js";   
+import path from "path";
+import { ENV } from "./src/lib/env.js";
 
 const app = express();
 
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
+const __dirname = path.resolve();
 
+// Health check
 app.get("/health", (req, res) => {
-  res.status(200).json({ msg: "API is up and runing" });
+  res.status(200).json({ msg: "API is up and running" });
 });
+
+// Example API
+app.get("/books", (req, res) => {
+  res.status(200).json({ msg: "This is the books endpoint" });
+});
+
+// Prepare app for production deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen(ENV.PORT, () =>
   console.log("Server is running on port:", ENV.PORT)
