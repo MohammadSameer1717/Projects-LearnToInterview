1edf069a50bcc3201f7e937c6437013ceed844e7
MohammadSameer1717 <mohammadsameer0893@gmail.com>
2025-11-18 01:06:37 +0530

    deployment config added

--- Backend/index.js
+++ Backend/index.js
@@ -5,18 +5,17 @@ import { ENV } from "./src/lib/env.js";
 const app = express();
 
 const __dirname = path.resolve();
-
-// Health check
+ 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "API is up and running" });
 });
 
-// Example API
+ 
 app.get("/books", (req, res) => {
   res.status(200).json({ msg: "This is the books endpoint" });
 });
 
-// Prepare app for production deployment
+ 
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
